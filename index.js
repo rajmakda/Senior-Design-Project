47a130b1aa6f85301020bfb49d8d9ce1cb0949e8
Raj Makda <raj@Rajs-MacBook-Pro.local>
2018-08-29 19:18:34 -0700

    Setting up express server and added dummy index route

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+
+// Requiring routes
+var indexRoute = require("./server/routes/index");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+const API_PORT = process.env.API_PORT || 3001;
+//Serving production build of React using Express
+app.use(express.static(__dirname + "/client/build"));
+
+// Dummy index route for API
+app.use("/api", indexRoute);
+
+// Route to send unmatched routes to React
+app.use("*", function(req,res) {
+    res.sendFile(__dirname + "client/build/index.html");
+})
+
+app.listen(API_PORT, function() {
+    console.log(`Listening on PORT ${API_PORT}`);
+})
