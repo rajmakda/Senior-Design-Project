09d5d6e7847a06e6d5619a3825cbf70436f82ef4
Raj Makda <raj@Rajs-MacBook-Pro.local>
2018-09-01 13:30:37 -0700

    Makes database url configurable for production and development

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const API_PORT = process.env.API_PORT || 3001;
 app.use(express.static(__dirname + "/client/build"));
 
 // Mongoose connection
-mongoose.connect(`mongodb+srv://${config.dbconfig.username}:${config.dbconfig.password}@cluster0-ymruv.mongodb.net/ihouseapp`, { useNewUrlParser: true });
+const dburl = process.env.DBURL || `mongodb+srv://${config.dbconfig.username}:${config.dbconfig.password}@cluster0-ymruv.mongodb.net/ihouseapp`;
+mongoose.connect(dburl, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error')).catch(err => {
     console.log(err);
