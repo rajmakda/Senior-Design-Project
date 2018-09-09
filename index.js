79ba17ee9345246ce0c005eb3a4ce7ccbf6d7fa6
Raj Makda <raj@Rajs-MacBook-Pro.local>
2018-09-09 16:38:55 -0700

    Added routes to get all timesheets for a given timeperiod and to submit a new timesheet to the database

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+mongoose.set("useCreateIndex", true);
 // Requiring configuration file
 var config = require("./config.js");
 
 // Requiring routes
 var indexRoute = require("./server/routes/index");
+var timesheetRoutes = require("./server/routes/Timesheet");
 
 var app = express();
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const API_PORT = process.env.API_PORT || 3001;
 //Serving production build of React using Express
@@ -16,18 +18,25 @@ app.use(express.static(__dirname + "/client/build"));
 
 // Mongoose connection
 const dburl = process.env.DBURL || `mongodb+srv://${config.dbconfig.username}:${config.dbconfig.password}@cluster0-ymruv.mongodb.net/ihouseapp`;
-mongoose.connect(dburl, { useNewUrlParser: true });
+mongoose.connect(dburl, { useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error')).catch(err => {
     console.log(err);
 })
 
+// Route for GIA Timesheets
+app.use("/api/timesheet",timesheetRoutes);
 // Dummy index route for API
 app.use("/api", indexRoute);
 
 // Route to send unmatched routes to React
 app.use("*", function(req,res) {
     res.sendFile(__dirname + "/client/build/index.html");
+})
+// Error handler
+app.use(function (err, req, res, next) {
+    console.error(err);
+    res.status(500).json({ err });
 })
 
 app.listen(API_PORT, function() {
