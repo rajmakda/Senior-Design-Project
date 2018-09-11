e08a4aa832c8a130053fe86cc953aa6375191dde
Raj Makda <raj@Rajs-MacBook-Pro.local>
2018-09-11 01:35:28 -0700

    Added register, login, logout end points. Used JWT tokens for further authorization to access of resources

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -19,7 +19,7 @@ class Login extends Component {
             email: this.state.email,
             password: this.state.password
         }
-        fetch('/api/login', {
+        fetch('/api/auth/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
@@ -40,13 +40,13 @@ class Login extends Component {
         return (
             <div className="container text-center">
                 <form className="form-signin" onSubmit={this.handleSubmit}>
-                    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                     <div className="form-group">
-                        <label for="email" className="sr-only">Email Address</label>
-                        <input type="email" name="email" className="form-control" value={this.state.email} placeholder="Enter Email" onChange={this.handleInputChange} required autofocus/>
+                        <label htmlFor="email" className="sr-only">Email Address</label>
+                        <input type="email" name="email" className="form-control" value={this.state.email} placeholder="Enter Email" onChange={this.handleInputChange} required autoFocus/>
                     </div>
                     <div className="form-group">
-                        <label for="password" className="sr-only">Password</label>
+                        <label htmlFor="password" className="sr-only">Password</label>
                         <input type="password" name="password" className="form-control" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} required/>
                     </div>
                     <button type="submit" className="btn btn-lg btn-primary btn-block">Submit</button>
